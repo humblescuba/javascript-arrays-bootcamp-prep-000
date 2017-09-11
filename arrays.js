d7c37e825e3dd6b1f974a109f16c6598fdf442af
Steve Olender <nantucket.friar@gmail.com>
2017-09-11 02:15:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,8 @@ var chocolateBars = [
 function addElementToBeginningOfArray(array, element) {
   return ['foo', ...[1]];
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = array.unshift('foo');
+  return array;
+}
